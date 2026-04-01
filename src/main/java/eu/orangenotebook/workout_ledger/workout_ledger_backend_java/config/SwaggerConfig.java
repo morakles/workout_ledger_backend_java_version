1d93cdcdbdc8eaf307f788c8c9720943de6ce24d
Dawid Morawiec <moralpoczta@gmail.com>
2026-04-01 16:17:55 +0200

    Add Swagger OpenAPI configuration class

---
+++ src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package eu.orangenotebook.workout_ledger.workout_ledger_backend_java.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI workoutLedgerOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Workout Ledger API")
+                        .description("API documentation for Workout Ledger backend.")
+                        .version("v1")
+                        .contact(new Contact().name("Workout Ledger Team"))
+                        .license(new License().name("Proprietary")));
+    }
+}
