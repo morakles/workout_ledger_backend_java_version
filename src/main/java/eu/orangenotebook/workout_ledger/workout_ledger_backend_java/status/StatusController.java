c0ff5f81f7d936cb2dab9faef4a7194e04e0196e
Moral <moralpoczta@gmail.com>
2026-04-01 15:55:36 +0200

    Commit

--- src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/status/StatusController.java
+++ src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/status/StatusController.java
@@ -3,20 +3,23 @@ package eu.orangenotebook.workout_ledger.workout_ledger_backend_java.status;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/status")
 public class StatusController {
 
     @GetMapping(produces = "application/json")
-    public ResponseEntity<String> status(){
-        return ResponseEntity.ok("=== OK ===");
+    public ResponseEntity<StatusResponse> status(){
+        return ResponseEntity.ok(new StatusResponse("OK", Instant.now()));
+    }
+
+    record StatusResponse(String status, Instant timestamp) {
+
     }
 }
+
+
 
