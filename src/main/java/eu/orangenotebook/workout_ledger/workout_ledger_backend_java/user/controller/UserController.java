906f1a0e25e71e408b8e4fe5ec076b944a1e2702
Moral <moralpoczta@gmail.com>
2026-04-02 09:41:06 +0200

    Add Maven wrapper scripts and gitattributes

--- src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/user/controller/UserController.java
+++ src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/user/controller/UserController.java
@@ -12,10 +12,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
@@ -39,8 +39,8 @@ public class UserController {
         }
     }
 
-    @GetMapping("/{email}")
-    public ResponseEntity<UserResponse> getUserByEmail(@PathVariable @Email String email) {
+    @GetMapping("/by-email")
+    public ResponseEntity<UserResponse> getUserByEmail(@RequestParam @NotBlank @Email String email) {
         return userService.getUserByEmail(email)
                 .map(user -> ResponseEntity.ok(UserResponse.from(user)))
                 .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
