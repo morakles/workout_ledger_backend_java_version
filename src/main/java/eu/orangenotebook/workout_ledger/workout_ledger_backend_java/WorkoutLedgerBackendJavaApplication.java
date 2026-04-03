94f38bd8231576c069f2c29e91ffc75cfa74fc32
Moral <moralpoczta@gmail.com>
2026-04-03 15:39:44 +0200

    Remove duplicate Mongo auditing enablement

--- src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/WorkoutLedgerBackendJavaApplication.java
+++ src/main/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/WorkoutLedgerBackendJavaApplication.java
@@ -2,10 +2,8 @@ package eu.orangenotebook.workout_ledger.workout_ledger_backend_java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
 public class WorkoutLedgerBackendJavaApplication {
 
     public static void main(String[] args) {
