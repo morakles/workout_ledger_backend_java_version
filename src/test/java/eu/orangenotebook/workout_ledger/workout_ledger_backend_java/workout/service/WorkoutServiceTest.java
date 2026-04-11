9b82d2c6e9e037e708e043a7fbd1ea984fec9a8d
Moral <moralpoczta@gmail.com>
2026-04-11 13:06:57 +0200

    Fixed tests

--- src/test/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/workout/service/WorkoutServiceTest.java
+++ src/test/java/eu/orangenotebook/workout_ledger/workout_ledger_backend_java/workout/service/WorkoutServiceTest.java
@@ -28,11 +28,8 @@ import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.InstanceOfAssertFactories.set;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class WorkoutServiceTest {
